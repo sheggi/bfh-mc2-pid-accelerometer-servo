4c29a3a9b17e96d33127b48a2af25f0329419b47
sheggi <thesheggi@gmail.com>
2019-09-27 21:27:16 +0200

    PID applied

---
+++ src/main/java/Main.java
@@ -0,0 +1,76 @@
+import com.tinkerforge.*;
+import com.tinkerforge.IPConnection;
+import com.tinkerforge.BrickletAccelerometer;
+
+public class Main {
+    private static final String HOST = "localhost";
+    private static final int PORT = 4223;
+
+    private static final String ACC_UID = "v8P";
+    private static final String SERVO_UID = "6rnbKm";
+    private static final String POTI_UID = "r67";
+    private static final short SERVO_NR = 6;
+
+
+    private static final boolean DEBUG = false;
+
+
+    // Note: To make the example code cleaner we do not handle exceptions. Exceptions
+    //       you might normally want to catch are described in the documentation
+    public static void main(String args[]) throws Exception {
+        // config tinkerforge
+        IPConnection ipcon = new IPConnection(); // Create IP connection
+
+        BrickletAccelerometer accelerometer = new BrickletAccelerometer(ACC_UID, ipcon);
+        BrickServo servo = new BrickServo(SERVO_UID, ipcon);
+        BrickletLinearPoti poti = new BrickletLinearPoti(POTI_UID, ipcon);
+
+        ipcon.connect(HOST, PORT); // Connect to brickd
+        // Don't use device before ipcon is connected
+
+        servo.enable(SERVO_NR);
+        servo.setAcceleration(SERVO_NR, 65535);
+
+        // config pid
+
+        PID pid = new PID();
+
+        pid.setPID(0.1, 0.00015, 2);
+        pid.setRequired(0);
+
+
+        // start loop
+
+        System.out.println("Start");
+
+        while (true) {
+            try {
+                // Get current acceleration
+                BrickletAccelerometer.Acceleration acceleration = accelerometer.getAcceleration();
+                double y = acceleration.y;
+                double potiRel = (poti.getAnalogValue() - 2047) / 10;
+
+                if(DEBUG) System.out.println("Acceleration [X]: \t" + acceleration.x / 1000.0 + " g \t [Y]: " + acceleration.y / 1000.0 + " g \t [Z]: " + acceleration.z / 1000.0 + " g");
+
+                pid.setInput(y);
+                double steuer = pid.getOutput();
+
+                // apply correction to absolute
+                double pos = servo.getPosition(SERVO_NR);
+                double newPos = pos - steuer; // invert correction to counter act influence
+
+                newPos += potiRel; // konstante störgrösse
+
+                servo.setPosition(SERVO_NR, (short) newPos);
+
+                if(true) System.out.printf("Error %f\tCorrection %f\tErrSum %f\tStör %f%n", pid.getError(), pid.getOutput(), pid.getErrorSum(), potiRel);
+
+                if(DEBUG) System.out.printf("Error %f\tSteuer %f\tPos %f\tNewPos %f\tP %f\tI %f\tD %f\tErrSum %f%n", pid.getError(), steuer, pos, newPos,pid.getProportional(), pid.getIntegral(), pid.getDifferencial(), pid.getErrorSum());
+
+            } catch (TimeoutException $ex) {
+                System.out.println('.');
+            }
+        }
+
+    }
+}
---
+++ src/main/java/PID.java
@@ -0,0 +1,54 @@
+public class PID {
+    private double errorSum = 0;
+    private double lastError = 0;
+    private double error = 0;
+    private double required = 0;
+
+    private double pFactor = 0;
+    private double iFactor = 0;
+    private double dFactor = 0;
+
+    public void setRequired(double r) {
+        this.required = r;
+    }
+
+    public void setPID(double p, double i, double d) {
+        this.pFactor = p;
+        this.iFactor = i;
+        this.dFactor = d;
+    }
+
+    public void setInput(double newActual) {
+        this.error = this.required - newActual;
+    }
+
+    public double getError() {
+        return this.error;
+    }
+
+    public double getErrorSum() {
+        return this.errorSum;
+    }
+
+    public double getProportional() {
+        return this.pFactor * this.error;
+    }
+
+    public double getIntegral() {
+        this.errorSum += this.error;
+        return this.iFactor * this.errorSum;
+    }
+
+    public double getDifferencial() {
+        double delta = this.lastError - this.error;
+        this.lastError = this.error;
+        return this.dFactor * delta;
+    }
+
+    public double getOutput() {
+        double p = this.getProportional();
+        double i = this.getIntegral();
+        double d = this.getDifferencial();
+        return p + i + d;
+    }
+}
